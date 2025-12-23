62363593b55f966556580475b4255ccdfd85ab51
sayuristy123-lang <sayuristy123@gmail.com>
2025-12-24 04:27:33 +0700

    Add files via upload

--- JS main.js
+++ JS main.js
@@ -1,21 +1,30 @@
-function validateCaptcha() {
-  let input = document.getElementById("captcha_input").value;
-  if (input !== captchaCode) {
-    alert("Captcha salah! Coba lagi.");
-    generateCaptcha();
-    return false;
-  }
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
-  // cek username dan password sederhana (contoh hardcode)
-  let username = document.getElementById("username").value;
-  let password = document.getElementById("password").value;
+const firebaseConfig = {
+  apiKey: "AIzaSyC9M1LcVIivLlNBDxaJRiV-7Lb0fLo3yrg",
+  authDomain: "web-apk-kepegawaian.firebaseapp.com",
+  databaseURL: "https://web-apk-kepegawaian-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "web-apk-kepegawaian",
+  storageBucket: "web-apk-kepegawaian.appspot.com",
+  messagingSenderId: "458664240424",
+  appId: "1:458664240424:web:096019e5f5f2ea83049c1d"
+};
 
-  if (username === "admin" && password === "12345") {
-    alert("Login berhasil!");
-    window.location.href = "dashboard.html"; // pindah ke halaman dashboard
-    return false; // supaya form tidak reload
-  } else {
-    alert("Username atau password salah!");
-    return false;
-  }
-}
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+// ASN
+onValue(ref(db, "pegawai/asn"), (snapshot) => {
+  const data = snapshot.val();
+  document.getElementById("jumlahASN").innerText =
+    data ? Object.keys(data).length : 0;
+});
+
+// NON ASN
+onValue(ref(db, "pegawai/non_asn"), (snapshot) => {
+  const data = snapshot.val();
+  document.getElementById("jumlahNonASN").innerText =
+    data ? Object.keys(data).length : 0;
+});
+
